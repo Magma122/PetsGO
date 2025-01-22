92891918608a44a1a8e7d3e90d6cb38efeec2f15
Magma122 <dooolooooo@gmail.com>
2025-01-22 20:53:27 +0700

    0.0.0

--- General.lua
+++ General.lua
@@ -1,20 +1,13 @@
 return function(Tabs, Options)
     local RollToggle = Tabs.General:AddToggle("AutoRoll", {
-        Title = "Mining Merchant Buy ALL", 
-        Description = "You can choose the ore to mine.",
+        Title = "Auto Roll Dice", 
         Default = false
     })
 
     RollToggle:OnChanged(function()
         while Options.AutoRoll.Value do
-            for i = 1, 8 do
-                local args = {
-                    [1] = "MiningMerchant",
-                    [2] = i
-                }
-                
-                pcall(game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("CustomMerchants_Purchase"):InvokeServer(unpack(args)))
-            end
+            game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Eggs_Roll"):InvokeServer()
+            -- Add potion
         end
     end)
 
