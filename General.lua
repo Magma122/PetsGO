e779242e5203674e5383befa0d4e732c72cd379f
Magma122 <dooolooooo@gmail.com>
2025-01-22 20:55:41 +0700

    0.0.0

--- General.lua
+++ General.lua
@@ -7,6 +7,7 @@ return function(Tabs, Options)
     RollToggle:OnChanged(function()
         while Options.AutoRoll.Value do
             game:GetService("ReplicatedStorage"):WaitForChild("Network"):WaitForChild("Eggs_Roll"):InvokeServer()
+            wait(0.01)
             -- Add potion
         end
     end)
